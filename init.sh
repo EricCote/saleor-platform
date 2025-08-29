5314c1fef8da9894249f707655b3f73d44c337e7
Eric Cote <eric@coteexpert.com>
2025-08-29 12:18:41 -0400

    Change the docker compose

---
+++ init.sh
@@ -0,0 +1,26 @@
+
+docker compose build
+docker compose pull
+docker compose run --rm api python3 manage.py migrate
+#docker compose run --rm api python3 manage.py populatedb
+docker compose run --rm api python3 manage.py createsuperuser --no-input
+docker compose run --rm storefront sh -c "HUSKY=0 pnpm install"
+docker compose up -d
+
+if [[ -v NVM_DIR ]]; then
+  echo "NVM is installed.  No need to install node."
+else
+  # Loads and installs nvm
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+  #refresh env variables
+  source ~/.bashrc
+
+  nvm install --lts
+  nvm use --lts
+fi
+
+cd CreateProducts
+node init.js
+node categories.js
+node menus.js
+node products.js
